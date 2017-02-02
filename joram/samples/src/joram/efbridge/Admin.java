f7b615670f47059de34c4d73081a7246a2c2bbfb
afreyssin <afreyssin@localhost>
2017-02-02 15:06:24 +0000

    Adds a bridge sample based on real client configuration (bridge / pivot architecture).

---
+++ joram/samples/src/joram/efbridge/Admin.java
@@ -0,0 +1,65 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2014 - 2017 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
+package efbridge;
+
+import javax.jms.ConnectionFactory;
+
+import org.objectweb.joram.client.jms.Queue;
+import org.objectweb.joram.client.jms.admin.AdminModule;
+import org.objectweb.joram.client.jms.admin.JMSAcquisitionQueue;
+import org.objectweb.joram.client.jms.admin.JMSDistributionQueue;
+import org.objectweb.joram.client.jms.admin.User;
+import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
+
+public class Admin {
+  public static void main(String[] args) throws Exception {
+    System.out.println();
+    System.out.println("Bridge administration...");
+
+    ConnectionFactory bridgeCF = TcpConnectionFactory.create("localhost", 16011);
+
+    AdminModule.connect(bridgeCF, "root", "root");
+    
+    User.create("anonymous", "anonymous");
+
+    // Creating a Queue Distribution bridge on server 1:
+    Queue distq = JMSDistributionQueue.create(1, "queue");
+    distq.setFreeWriting();
+    System.out.println("joram distribution queue = " + distq);
+
+    // Creating a Queue Acquisition bridge on server 1:
+    Queue acqq = JMSAcquisitionQueue.create(1, "queue");
+    acqq.setFreeReading();
+    System.out.println("joram acquisition queue = " + acqq);
+    
+    // bind foreign destination and connectionFactory
+    javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+    jndiCtx.rebind("distq", distq);
+    jndiCtx.rebind("acqq", acqq);
+    jndiCtx.rebind("bridgeCF", bridgeCF);
+    jndiCtx.close();
+
+    AdminModule.disconnect();
+    System.out.println("Admin closed.");
+  }
+}
