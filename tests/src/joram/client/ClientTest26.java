be5c99687b0c927360b66fa3092742665db4e689
afreyssin <afreyssin@localhost>
2017-10-04 12:01:22 +0000

    Minor changes.

--- tests/src/joram/client/ClientTest26.java
+++ tests/src/joram/client/ClientTest26.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2012 - ScalAgent Distributed Technologies
+ * Copyright (C) 2012 - 2017 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
