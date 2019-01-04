b1fc1e4ae22b19fc4277a265c0d26bc5564757c7
freyssin <andre.freyssinet@scalagent.com>
2019-01-04 17:15:21 +0100

    Fix JORAM-316: Due to the use of Form parameters for JMS authentication information, the content-type of the POST shall be fixed.

--- joram/samples/src/joram/rest/RestProducer.java
+++ joram/samples/src/joram/rest/RestProducer.java
@@ -1,3 +1,25 @@
+/*
+ * JORAM: Java(TM) Open Reliable Asynchronous Messaging
+ * Copyright (C) 2018 - 2019 ScalAgent Distributed Technologies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA.
+ *
+ * Initial developer(s): ScalAgent Distributed Technologies
+ * Contributor(s): 
+ */
 package rest;
 
 import java.net.URI;
@@ -47,11 +69,11 @@ public class RestProducer {
 //      .queryParam("name", "prod1")
         .getUri();
 
-    
+    System.out.println("URI -> " + uriCreateProd);
     // Create the producer
     response = client.target(uriCreateProd)
         .request()
-        .accept(MediaType.TEXT_PLAIN).post(null);
+        .accept(MediaType.TEXT_PLAIN).post(Entity.entity(null, MediaType.APPLICATION_FORM_URLENCODED));
 
     uriCloseProd = response.getLink("close-context").getUri();
     uriSendNextMsg = response.getLink("send-next-message").getUri();
