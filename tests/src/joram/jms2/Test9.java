f4be87fa3c8d209cbb50dbf9691e3735d8aa71a1
freyssin <andre.freyssinet@scalagent.com>
2019-01-23 10:12:11 +0100

    Adds required ClientID for durable subscriptions.

--- tests/src/joram/jms2/Test9.java
+++ tests/src/joram/jms2/Test9.java
@@ -1,6 +1,6 @@
 /*
  * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2013 ScalAgent Distributed Technologies
+ * Copyright (C) 2013 - 2019 ScalAgent Distributed Technologies
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -63,6 +63,7 @@ public class Test9 extends TestCase {
       AdminModule.disconnect();
 
       JMSContext context = cf.createContext();
+      context.setClientID("MyClientID");
       JMSProducer producer = context.createProducer();
       JMSConsumer consumer = context.createDurableConsumer(topic, "dursub", "lastMessage = TRUE", false);
 
