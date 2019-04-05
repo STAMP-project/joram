a009003a335686413c0a7d9425a45f7ff970cd35
Jenkins <jenkins@Debian8-CI-LH01.cs1cloud.internal>
2019-04-05 16:38:09 +0200

    added tests

--- joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/notifications/TopicForwardNotTest.java
+++ joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/notifications/TopicForwardNotTest.java
@@ -1,79 +1,111 @@
-/*
- * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2013 ScalAgent Distributed Technologies
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA.
- *
- * Initial developer(s): ScalAgent Distributed Technologies
- * Contributor(s):
- */
 package org.objectweb.joram.mom.notifications;
 
-import junit.framework.Assert;
-
-import org.junit.Test;
-import org.objectweb.joram.mom.proxies.EncodingHelper;
-
-import fr.dyade.aaa.agent.AgentId;
 
 public class TopicForwardNotTest {
-  
-  public static final String PROPERTY1 = "property1";
-  public static final String PROPERTY2 = "property2";
-  public static final String PROPERTY3 = "property3";
-  public static final String PROPERTY4 = "property4";
+    public static final java.lang.String PROPERTY1 = "property1";
 
-  @Test
-  public void run() throws Exception {
-    EncodingHelper.init();
-    
-    org.objectweb.joram.shared.messages.Message sharedMsg = new org.objectweb.joram.shared.messages.Message();
-    sharedMsg.id = "msgId";
-    sharedMsg.toId = "toId";
-    sharedMsg.toName = null;
-    sharedMsg.toType = 1;
-    sharedMsg.timestamp = 17899823;
-    sharedMsg.compressed = true;
-    sharedMsg.deliveryTime = 100;
-    sharedMsg.body = null;
-    sharedMsg.clientID = null;
+    public static final java.lang.String PROPERTY2 = "property2";
 
-    sharedMsg.toName = "toName"; // null
-    sharedMsg.body = new byte[1000];  // null
-    sharedMsg.clientID = "clientId"; // null
-    
-    ClientMessages clientMessages = new ClientMessages(1, 2);
-    clientMessages.addMessage(sharedMsg);
-    clientMessages.setProxyId(new AgentId((short)1, (short)2, 3));
-    
-    TopicForwardNot topicForwardNot = new TopicForwardNot(clientMessages, false);
-    checkEncoding(topicForwardNot);
-  }
+    public static final java.lang.String PROPERTY3 = "property3";
 
-  private void checkEncoding(TopicForwardNot topicForwardNot) throws Exception {
-    byte[] bytes = EncodingHelper.encode(topicForwardNot);
+    public static final java.lang.String PROPERTY4 = "property4";
 
-    TopicForwardNot dec = (TopicForwardNot) EncodingHelper.decode(
-        topicForwardNot.getEncodableClassId(), bytes);
+    @org.junit.Test(timeout = 10000)
+    public void run() throws java.lang.Exception {
+        org.objectweb.joram.mom.proxies.EncodingHelper.init();
+        org.objectweb.joram.shared.messages.Message sharedMsg = new org.objectweb.joram.shared.messages.Message();
+        sharedMsg.id = "msgId";
+        org.junit.Assert.assertEquals("msgId", sharedMsg.id);
+        sharedMsg.toId = "toId";
+        org.junit.Assert.assertEquals("toId", sharedMsg.toId);
+        sharedMsg.toName = null;
+        org.junit.Assert.assertNull(sharedMsg.toName);
+        sharedMsg.toType = 1;
+        sharedMsg.timestamp = 17899823;
+        sharedMsg.compressed = true;
+        sharedMsg.deliveryTime = 100;
+        sharedMsg.body = null;
+        sharedMsg.clientID = null;
+        org.junit.Assert.assertNull(sharedMsg.clientID);
+        sharedMsg.toName = "toName";
+        org.junit.Assert.assertEquals("toName", sharedMsg.toName);
+        sharedMsg.body = new byte[1000];
+        sharedMsg.clientID = "clientId";
+        org.junit.Assert.assertEquals("clientId", sharedMsg.clientID);
+        org.objectweb.joram.mom.notifications.ClientMessages clientMessages = new org.objectweb.joram.mom.notifications.ClientMessages(1, 2);
+        org.junit.Assert.assertEquals(0, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessageCount())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getDMQId());
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getAsyncSend());
+        org.junit.Assert.assertTrue(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessages().isEmpty());
+        org.junit.Assert.assertEquals(131082, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getEncodableClassId())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId());
+        org.junit.Assert.assertEquals(13, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getEncodedSize())));
+        org.junit.Assert.assertEquals(2, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getRequestId())));
+        org.junit.Assert.assertEquals(1, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getClientContext())));
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).hasCallback());
+        org.junit.Assert.assertEquals(0L, ((long) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getExpiration())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessageId());
+        org.junit.Assert.assertTrue(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).isPersistent());
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getDeadNotificationAgentId());
+        org.junit.Assert.assertEquals(4, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getPriority())));
+        clientMessages.addMessage(sharedMsg);
+        clientMessages.setProxyId(new fr.dyade.aaa.agent.AgentId(((short) (1)), ((short) (2)), 3));
+        org.objectweb.joram.mom.notifications.TopicForwardNot topicForwardNot = new org.objectweb.joram.mom.notifications.TopicForwardNot(clientMessages, false);
+        org.junit.Assert.assertEquals(131081, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getEncodableClassId())));
+        org.junit.Assert.assertEquals(1093, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getEncodedSize())));
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).hasCallback());
+        org.junit.Assert.assertEquals(0L, ((long) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getExpiration())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getMessageId());
+        org.junit.Assert.assertTrue(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).isPersistent());
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getDeadNotificationAgentId());
+        org.junit.Assert.assertEquals(4, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getPriority())));
+        checkEncoding(topicForwardNot);
+        org.junit.Assert.assertEquals("msgId", sharedMsg.id);
+        org.junit.Assert.assertEquals("toId", sharedMsg.toId);
+        org.junit.Assert.assertEquals("toName", sharedMsg.toName);
+        org.junit.Assert.assertEquals("clientId", sharedMsg.clientID);
+        org.junit.Assert.assertEquals("toName", sharedMsg.toName);
+        org.junit.Assert.assertEquals("clientId", sharedMsg.clientID);
+        org.junit.Assert.assertEquals(1, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessageCount())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getDMQId());
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getAsyncSend());
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessages().isEmpty());
+        org.junit.Assert.assertEquals(131082, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getEncodableClassId())));
+        org.junit.Assert.assertEquals(2, ((short) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).getTo())));
+        org.junit.Assert.assertEquals(1, ((short) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).getFrom())));
+        org.junit.Assert.assertEquals(3, ((int) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).getStamp())));
+        org.junit.Assert.assertFalse(((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).isNullId());
+        org.junit.Assert.assertFalse(((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).isLocal());
+        org.junit.Assert.assertEquals(-1, ((int) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).getEncodableClassId())));
+        org.junit.Assert.assertEquals(8, ((int) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).getEncodedSize())));
+        org.junit.Assert.assertEquals("#1.2.3", ((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).toString());
+        org.junit.Assert.assertEquals(3, ((int) (((fr.dyade.aaa.agent.AgentId) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getProxyId())).hashCode())));
+        org.junit.Assert.assertEquals(1087, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getEncodedSize())));
+        org.junit.Assert.assertEquals(2, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getRequestId())));
+        org.junit.Assert.assertEquals(1, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getClientContext())));
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).hasCallback());
+        org.junit.Assert.assertEquals(0L, ((long) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getExpiration())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getMessageId());
+        org.junit.Assert.assertTrue(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).isPersistent());
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getDeadNotificationAgentId());
+        org.junit.Assert.assertEquals(4, ((int) (((org.objectweb.joram.mom.notifications.ClientMessages) (clientMessages)).getPriority())));
+        org.junit.Assert.assertEquals(131081, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getEncodableClassId())));
+        org.junit.Assert.assertEquals(1093, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getEncodedSize())));
+        org.junit.Assert.assertFalse(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).hasCallback());
+        org.junit.Assert.assertEquals(0L, ((long) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getExpiration())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getMessageId());
+        org.junit.Assert.assertTrue(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).isPersistent());
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getDeadNotificationAgentId());
+        org.junit.Assert.assertEquals(4, ((int) (((org.objectweb.joram.mom.notifications.TopicForwardNot) (topicForwardNot)).getPriority())));
+    }
 
-    ClientMessages cm = topicForwardNot.messages;
-
-    Assert.assertEquals(topicForwardNot.fromCluster, dec.fromCluster);
-    Assert.assertEquals(topicForwardNot.fromCluster, dec.fromCluster);
-    Assert.assertEquals(cm.getProxyId(), dec.messages.getProxyId());
-  }
-
+    private void checkEncoding(org.objectweb.joram.mom.notifications.TopicForwardNot topicForwardNot) throws java.lang.Exception {
+        byte[] bytes = org.objectweb.joram.mom.proxies.EncodingHelper.encode(topicForwardNot);
+        org.objectweb.joram.mom.notifications.TopicForwardNot dec = ((org.objectweb.joram.mom.notifications.TopicForwardNot) (org.objectweb.joram.mom.proxies.EncodingHelper.decode(topicForwardNot.getEncodableClassId(), bytes)));
+        org.objectweb.joram.mom.notifications.ClientMessages cm = topicForwardNot.messages;
+        junit.framework.Assert.assertEquals(topicForwardNot.fromCluster, dec.fromCluster);
+        junit.framework.Assert.assertEquals(topicForwardNot.fromCluster, dec.fromCluster);
+        junit.framework.Assert.assertEquals(cm.getProxyId(), dec.messages.getProxyId());
+    }
 }
+
--- joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/proxies/ClientContextEncodingTest.java
+++ joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/proxies/ClientContextEncodingTest.java
@@ -1,81 +1,56 @@
-/*
- * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2013 ScalAgent Distributed Technologies
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA.
- *
- * Initial developer(s): ScalAgent Distributed Technologies
- * Contributor(s):
- */
 package org.objectweb.joram.mom.proxies;
 
-import junit.framework.Assert;
-
-import org.junit.Test;
-
-import fr.dyade.aaa.agent.AgentId;
 
 public class ClientContextEncodingTest {
-  
-  @Test
-  public void run() throws Exception {
-    EncodingHelper.init();
-    
-    // Not encoded by ClientContext
-    AgentId proxyId = null;
-    int id = 40;
+    @org.junit.Test(timeout = 10000)
+    public void run() throws java.lang.Exception {
+        org.objectweb.joram.mom.proxies.EncodingHelper.init();
+        fr.dyade.aaa.agent.AgentId proxyId = null;
+        int id = 40;
+        org.objectweb.joram.mom.proxies.ClientContext cc1 = new org.objectweb.joram.mom.proxies.ClientContext(proxyId, id);
+        org.junit.Assert.assertEquals(131076, ((int) (((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getEncodableClassId())));
+        org.junit.Assert.assertEquals(17, ((int) (((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getEncodedSize())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getProxyId());
+        org.junit.Assert.assertEquals("ClientContext (proxyId=null,id=40,tempDestinations=[],deliveringQueues={},transactionsTable=null,started=false,cancelledRequestId=-1,activeSubs=[],repliesBuffer=[])", ((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).toString());
+        boolean o_run__7 = cc1.getActiveSubList().add("sub1");
+        org.junit.Assert.assertTrue(o_run__7);
+        boolean o_run__9 = cc1.getActiveSubList().add("sub2");
+        org.junit.Assert.assertTrue(o_run__9);
+        fr.dyade.aaa.agent.AgentId queue1 = new fr.dyade.aaa.agent.AgentId(((short) (50)), ((short) (60)), 70);
+        fr.dyade.aaa.agent.AgentId o_run__13 = cc1.getDeliveringQueueTable().put(queue1, queue1);
+        org.junit.Assert.assertNull(o_run__13);
+        fr.dyade.aaa.agent.AgentId queue2 = new fr.dyade.aaa.agent.AgentId(((short) (80)), ((short) (90)), 100);
+        fr.dyade.aaa.agent.AgentId o_run__17 = cc1.getDeliveringQueueTable().put(queue2, queue2);
+        org.junit.Assert.assertNull(o_run__17);
+        fr.dyade.aaa.agent.AgentId tmpDest1 = new fr.dyade.aaa.agent.AgentId(((short) (110)), ((short) (120)), 130);
+        boolean o_run__21 = cc1.getTempDestinationList().add(tmpDest1);
+        org.junit.Assert.assertTrue(o_run__21);
+        fr.dyade.aaa.agent.AgentId tmpDest2 = new fr.dyade.aaa.agent.AgentId(((short) (120)), ((short) (130)), 140);
+        boolean o_run__25 = cc1.getTempDestinationList().add(tmpDest2);
+        org.junit.Assert.assertTrue(o_run__25);
+        checkEncoding(cc1);
+        org.junit.Assert.assertEquals(131076, ((int) (((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getEncodableClassId())));
+        org.junit.Assert.assertEquals(65, ((int) (((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getEncodedSize())));
+        org.junit.Assert.assertNull(((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).getProxyId());
+        org.junit.Assert.assertEquals("ClientContext (proxyId=null,id=40,tempDestinations=[#110.120.130, #120.130.140],deliveringQueues={#50.60.70=#50.60.70, #80.90.100=#80.90.100},transactionsTable=null,started=false,cancelledRequestId=-1,activeSubs=[sub1, sub2],repliesBuffer=[])", ((org.objectweb.joram.mom.proxies.ClientContext) (cc1)).toString());
+        org.junit.Assert.assertTrue(o_run__7);
+        org.junit.Assert.assertTrue(o_run__9);
+        org.junit.Assert.assertNull(o_run__13);
+        org.junit.Assert.assertNull(o_run__17);
+        org.junit.Assert.assertTrue(o_run__21);
+        org.junit.Assert.assertTrue(o_run__25);
+    }
 
-    ClientContext cc1 = new ClientContext(proxyId, id);
-    
-    cc1.getActiveSubList().add("sub1");
-    cc1.getActiveSubList().add("sub2");
-    
-    AgentId queue1 = new AgentId((short) 50, (short) 60, 70);
-    cc1.getDeliveringQueueTable().put(queue1, queue1);
-    AgentId queue2 = new AgentId((short) 80, (short) 90, 100);
-    cc1.getDeliveringQueueTable().put(queue2, queue2);
-    
-    AgentId tmpDest1 = new AgentId((short) 110, (short) 120, 130);
-    cc1.getTempDestinationList().add(tmpDest1);
-    AgentId tmpDest2 = new AgentId((short) 120, (short) 130, 140);
-    cc1.getTempDestinationList().add(tmpDest2);
+    private void checkEncoding(org.objectweb.joram.mom.proxies.ClientContext cc) throws java.lang.Exception {
+        byte[] bytes = org.objectweb.joram.mom.proxies.EncodingHelper.encode(cc);
+        org.objectweb.joram.mom.proxies.ClientContext ccDec = ((org.objectweb.joram.mom.proxies.ClientContext) (org.objectweb.joram.mom.proxies.EncodingHelper.decode(cc.getEncodableClassId(), bytes)));
+        junit.framework.Assert.assertEquals(cc.getId(), ccDec.getId());
+        junit.framework.Assert.assertEquals(cc.getActiveSubList(), ccDec.getActiveSubList());
+        junit.framework.Assert.assertEquals(cc.getDeliveringQueueTable(), ccDec.getDeliveringQueueTable());
+        junit.framework.Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
+        junit.framework.Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
 
-    // TODO: test XA
-    /*
-    Xid xid1 = new Xid("bq1".getBytes(), 150, "gti".getBytes());
-    XACnxPrepare prep1 = new XACnxPrepare(xid1.bq, xid1.fi, xid1.gti, null, null);
-    cc1.getTransactionsTable().put(xid1, prep1);*/
-    
-    checkEncoding(cc1);
-  }
-  
-  private void checkEncoding(ClientContext cc) throws Exception {
-    byte[] bytes = EncodingHelper.encode(cc);
 
-    ClientContext ccDec = (ClientContext) EncodingHelper.decode(
-        cc.getEncodableClassId(), bytes);
-
-    Assert.assertEquals(cc.getId(), ccDec.getId());
-    Assert.assertEquals(cc.getActiveSubList(), ccDec.getActiveSubList());
-    Assert.assertEquals(cc.getDeliveringQueueTable(), ccDec.getDeliveringQueueTable());
-    Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
-   Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
-
-    // TODO: test XA
-    //Assert.assertEquals(cc.getTransactionsTable(), ccDec.getTransactionsTable());
-  }
-
+    }
 }
+
