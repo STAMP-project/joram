f78cd6a233f6874e95f49a9ff6ee8ded0ef1bac7
Jenkins <jenkins@Debian8-CI-LH01.cs1cloud.internal>
2020-01-28 11:27:49 +0100

    added tests

--- joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/proxies/ClientContextEncodingTest.java
+++ joram/joram/mom/core/src/test/java/org/objectweb/joram/mom/proxies/ClientContextEncodingTest.java
@@ -1,82 +1,66 @@
-/*
- * JORAM: Java(TM) Open Reliable Asynchronous Messaging
- * Copyright (C) 2013 ScalAgent Distributed Technologies
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA.
- *
- * Initial developer(s): ScalAgent Distributed Technologies
- * Contributor(s):
- */
 package org.objectweb.joram.mom.proxies;
 
-import junit.framework.Assert;
-
-import org.junit.Test;
 
 import fr.dyade.aaa.agent.AgentId;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 
 public class ClientContextEncodingTest {
-  
-  @Test
-  public void run() throws Exception {
-    EncodingHelper.init();
-    
-    // Not encoded by ClientContext
-    AgentId proxyId = null;
-    int id = 40;
+    @Test(timeout = 10000)
+    public void run() throws Exception {
+        EncodingHelper.init();
+        AgentId proxyId = null;
+        int id = 40;
+        ClientContext cc1 = new ClientContext(proxyId, id);
+        Assert.assertEquals(131076, ((int) (((ClientContext) (cc1)).getEncodableClassId())));
+        Assert.assertNull(((ClientContext) (cc1)).getProxyId());
+        Assert.assertEquals(17, ((int) (((ClientContext) (cc1)).getEncodedSize())));
+        Assert.assertEquals("ClientContext (proxyId=null,id=40,tempDestinations=[],deliveringQueues={},transactionsTable=null,started=false,cancelledRequestId=-1,activeSubs=[],repliesBuffer=[])", ((ClientContext) (cc1)).toString());
+        boolean o_run__7 = cc1.getActiveSubList().add("sub1");
+        Assert.assertTrue(o_run__7);
+        boolean o_run__9 = cc1.getActiveSubList().add("sub2");
+        Assert.assertTrue(o_run__9);
+        boolean o_run__11 = cc1.getActiveSubList().add("sub2");
+        Assert.assertTrue(o_run__11);
+        AgentId queue1 = new AgentId(((short) (50)), ((short) (60)), 70);
+        AgentId o_run__15 = cc1.getDeliveringQueueTable().put(queue1, queue1);
+        Assert.assertNull(o_run__15);
+        AgentId queue2 = new AgentId(((short) (80)), ((short) (90)), 100);
+        AgentId o_run__19 = cc1.getDeliveringQueueTable().put(queue2, queue2);
+        Assert.assertNull(o_run__19);
+        AgentId tmpDest1 = new AgentId(((short) (110)), ((short) (120)), 130);
+        boolean o_run__23 = cc1.getTempDestinationList().add(tmpDest1);
+        Assert.assertTrue(o_run__23);
+        AgentId tmpDest2 = new AgentId(((short) (120)), ((short) (130)), 140);
+        boolean o_run__27 = cc1.getTempDestinationList().add(tmpDest2);
+        Assert.assertTrue(o_run__27);
+        checkEncoding(cc1);
+        Assert.assertEquals(131076, ((int) (((ClientContext) (cc1)).getEncodableClassId())));
+        Assert.assertNull(((ClientContext) (cc1)).getProxyId());
+        Assert.assertEquals(73, ((int) (((ClientContext) (cc1)).getEncodedSize())));
+        Assert.assertEquals("ClientContext (proxyId=null,id=40,tempDestinations=[#110.120.130, #120.130.140],deliveringQueues={#50.60.70=#50.60.70, #80.90.100=#80.90.100},transactionsTable=null,started=false,cancelledRequestId=-1,activeSubs=[sub1, sub2, sub2],repliesBuffer=[])", ((ClientContext) (cc1)).toString());
+        Assert.assertTrue(o_run__7);
+        Assert.assertTrue(o_run__9);
+        Assert.assertTrue(o_run__11);
+        Assert.assertNull(o_run__15);
+        Assert.assertNull(o_run__19);
+        Assert.assertTrue(o_run__23);
+        Assert.assertTrue(o_run__27);
+    }
 
-    ClientContext cc1 = new ClientContext(proxyId, id);
-    
-    cc1.getActiveSubList().add("sub1");
-    cc1.getActiveSubList().add("sub2");
-    cc1.getActiveSubList().add("sub2");
-    
-    AgentId queue1 = new AgentId((short) 50, (short) 60, 70);
-    cc1.getDeliveringQueueTable().put(queue1, queue1);
-    AgentId queue2 = new AgentId((short) 80, (short) 90, 100);
-    cc1.getDeliveringQueueTable().put(queue2, queue2);
-    
-    AgentId tmpDest1 = new AgentId((short) 110, (short) 120, 130);
-    cc1.getTempDestinationList().add(tmpDest1);
-    AgentId tmpDest2 = new AgentId((short) 120, (short) 130, 140);
-    cc1.getTempDestinationList().add(tmpDest2);
+    private void checkEncoding(ClientContext cc) throws Exception {
+        byte[] bytes = EncodingHelper.encode(cc);
+        ClientContext ccDec = ((ClientContext) (EncodingHelper.decode(cc.getEncodableClassId(), bytes)));
+        junit.framework.Assert.assertEquals(cc.getId(), ccDec.getId());
+        assertEquals(cc.getActiveSubList(), ccDec.getActiveSubList());
+        assertEquals(cc.getDeliveringQueueTable(), ccDec.getDeliveringQueueTable());
+        assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
+        assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
 
-    // TODO: test XA
-    /*
-    Xid xid1 = new Xid("bq1".getBytes(), 150, "gti".getBytes());
-    XACnxPrepare prep1 = new XACnxPrepare(xid1.bq, xid1.fi, xid1.gti, null, null);
-    cc1.getTransactionsTable().put(xid1, prep1);*/
-    
-    checkEncoding(cc1);
-  }
-  
-  private void checkEncoding(ClientContext cc) throws Exception {
-    byte[] bytes = EncodingHelper.encode(cc);
 
-    ClientContext ccDec = (ClientContext) EncodingHelper.decode(
-        cc.getEncodableClassId(), bytes);
-
-    Assert.assertEquals(cc.getId(), ccDec.getId());
-    Assert.assertEquals(cc.getActiveSubList(), ccDec.getActiveSubList());
-    Assert.assertEquals(cc.getDeliveringQueueTable(), ccDec.getDeliveringQueueTable());
-    Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
-   Assert.assertEquals(cc.getTempDestinationList(), ccDec.getTempDestinationList());
-
-    // TODO: test XA
-    //Assert.assertEquals(cc.getTransactionsTable(), ccDec.getTransactionsTable());
-  }
-
+    }
 }
+
